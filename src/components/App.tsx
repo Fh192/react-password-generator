322103e093f69f6824bb3081b6de5d05f9fe8748
Kirill Shevchenko <stranger.net3@gmail.com>
2021-10-24 02:11:45 +0400

    Deploy

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { copyPassword } from '../common/functions';
+import { useSelector } from '../hook/useSelector';
 import './App.css';
+import { GeneratedPassword } from './GeneratedPassword/GeneratedPassword';
+import { PasswordSettings } from './PasswordSettings/PasswordSettings';
 
 const App: React.FC = () => {
-  return <div className='App'>
-    <div className="container"></div>
-  </div>;
+  const { password } = useSelector(s => s.password);
+  const [scaleBtn, setScaleBtn] = useState(false);
+
+  return (
+    <div className='App'>
+      <div className='container'>
+        <div className='subtitle'>
+          <h2>PASSWORD GENERATOR TOOL</h2>
+        </div>
+        <div className='title'>
+          <h1>Generate a secure password</h1>
+        </div>
+        <div className='main'>
+          <GeneratedPassword />
+          <PasswordSettings />
+        </div>
+        <div
+          className={`copy ${scaleBtn ? 'copyScale' : ''}`}
+          onClick={() => {
+            setScaleBtn(true);
+            copyPassword(password);
+          }}
+          onAnimationEnd={() => setScaleBtn(false)}
+        >
+          <button>Copy password</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
