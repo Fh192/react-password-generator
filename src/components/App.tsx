a81bc91d0e6bbb975d08d0462010d9fc8127619d
Kirill Shevchenko <stranger.net3@gmail.com>
2021-10-23 14:16:05 +0400

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return <div className='App'>
+    <div className="container"></div>
+  </div>;
+};
+
+export default App;
